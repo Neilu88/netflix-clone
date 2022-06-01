ddbd03c392540320476f8176816016e2e2d1a5d6
Neil <neildatta8888@gmail.com>
2022-06-01 18:42:47 -0400

    Fixed errors

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,4 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
-import { DocumentData } from 'firebase/firestore'
 import { useRef, useState } from 'react'
 import { Movie } from '../typings'
 import Thumbnail from './Thumbnail'
@@ -42,8 +41,9 @@ function Row({ title, movies }: Props) {
           className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
           ref={rowRef}
         >
-          {movies.length &&
-            movies.map((movie) => <Thumbnail key={movie.id} movie={movie} />)}
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
         </div>
         <ChevronRightIcon
           className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
